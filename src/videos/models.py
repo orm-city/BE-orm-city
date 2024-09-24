2f4f2244863b7fc10eb961cba9d5289429bdc489
KyungMin.Ko <rhrudals312@naver.com>
2024-09-24 11:18:19 +0900

    ORMC-15 video 모델 작성

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+
+
+class Video(models.Model):
+    """
+    Video에 대한 모델 정의
+    """
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    video_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
