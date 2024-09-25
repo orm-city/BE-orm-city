6097c33efe1c037ee013d2e454428e72d561a0e6
KyungMin.Ko <rhrudals312@naver.com>
2024-09-25 11:57:25 +0900

    ORMC-22 video[views, urls] 작성 (video crud, progress 저장)

--- src/videos/views.py
+++ src/videos/views.py
@@ -1,92 +1,76 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-from .models import Video, VideoWatchProgress
-from .serializers import VideoWatchProgressSerializer
-from django.http import StreamingHttpResponse
-import requests
+from django.utils import timezone
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+# from rest_framework.permissions import IsAuthenticated
+
+from progress.models import UserProgress
+from progress.serializers import UserProgressSerializer
+
+from .models import Video
+from .serializers import VideoSerializer
 
 
-class VideoViewSet(viewsets.ViewSet):
-    """
-    비디오 스트리밍 및 시청 기록 관리
-    """
+class VideoCreateAPIView(generics.CreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
 
-    def retrieve(self, request, pk=None):
-        """
-        비디오 스트리밍 기능 구현
-        """
-        video = get_object_or_404(Video, pk=pk)
-        video_url = video.video_url
 
-        # 비디오 URL을 통해 스트리밍 응답 생성
-        def stream_video(video_url):
-            r = requests.get(video_url, stream=True)
-            for chunk in r.iter_content(chunk_size=8192):
-                yield chunk
+class VideoRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
 
-        response = StreamingHttpResponse(
-            stream_video(video_url), content_type="video/mp4"
-        )
-        response["Accept-Ranges"] = "bytes"
+    def get_queryset(self):
+        return Video.objects.filter(id=self.kwargs["pk"])
 
-        return response
 
-    @action(detail=True, methods=["get"], url_path="watch-progress")
-    def get_watch_progress(self, request, pk=None):
-        """
-        비디오의 시청 진행률을 반환
-        """
-        video = get_object_or_404(Video, pk=pk)
-        watch_progress = VideoWatchProgress.objects.filter(
+class VideoUpdateAPIView(generics.UpdateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def get_queryset(self):
+        return Video.objects.filter(id=self.kwargs["pk"])
+
+
+class VideoDeleteAPIView(generics.DestroyAPIView):
+    queryset = Video.objects.all()
+
+    def get_queryset(self):
+        return Video.objects.filter(id=self.kwargs["pk"])
+
+
+class UserProgressUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserProgressSerializer
+
+    def put(self, request, *args, **kwargs):
+        video_id = request.data.get("video_id")
+        new_progress = request.data.get("progress_percent")
+        additional_time = request.data.get("time_spent", 0)
+
+        video = get_object_or_404(Video, id=video_id)
+        user_progress, created = UserProgress.objects.get_or_create(
             user=request.user, video=video
-        ).first()
-
-        if watch_progress:
-            serializer = VideoWatchProgressSerializer(watch_progress)
-            return Response(serializer.data)
-        return Response(
-            {"message": "No watch progress found."}, status=status.HTTP_404_NOT_FOUND
         )
 
-    @action(detail=True, methods=["post"], url_path="watch-progress")
-    def save_watch_progress(self, request, pk=None):
-        """
-        비디오 시청 시간 및 진행률을 기록
-        """
-        video = get_object_or_404(Video, pk=pk)
-        serializer = VideoWatchProgressSerializer(data=request.data)
-        if serializer.is_valid():
-            watch_progress, created = VideoWatchProgress.objects.update_or_create(
-                user=request.user,
-                video=video,
-                defaults={
-                    "last_watched_time": serializer.validated_data["last_watched_time"],
-                    "progress": serializer.validated_data["progress"],
-                },
-            )
-            return Response(VideoWatchProgressSerializer(watch_progress).data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user_progress.update_progress(
+            new_progress, timezone.timedelta(seconds=additional_time)
+        )
 
-    @action(detail=True, methods=["get"], url_path="resume")
-    def resume_video(self, request, pk=None):
-        """
-        마지막 시청 지점을 가져와서 이어보기 기능을 구현
-        """
-        video = get_object_or_404(Video, pk=pk)
-        watch_progress = VideoWatchProgress.objects.filter(
-            user=request.user, video=video
-        ).first()
-
-        if watch_progress:
-            return Response(
-                {
-                    "resume_time": watch_progress.last_watched_time,
-                    "progress": watch_progress.progress,
-                    "message": "You can resume the video from the last watched point.",
-                }
-            )
         return Response(
-            {"message": "No resume point found."}, status=status.HTTP_404_NOT_FOUND
+            UserProgressSerializer(user_progress).data, status=status.HTTP_200_OK
+        )
+
+
+class UserProgressDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = UserProgressSerializer
+
+    def get(self, request, *args, **kwargs):
+        video_id = self.kwargs.get("video_id")
+        video = get_object_or_404(Video, id=video_id)
+        user_progress = get_object_or_404(UserProgress, user=request.user, video=video)
+
+        return Response(
+            UserProgressSerializer(user_progress).data, status=status.HTTP_200_OK
         )
