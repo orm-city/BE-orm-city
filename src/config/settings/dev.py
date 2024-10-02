e27985eb8f7a9f999b78c5592b53dd01352bd854
Jang JiWoo <77304817+cheeou@users.noreply.github.com>
2024-10-03 00:45:21 +0900

    dev Payment앱 verify_iamport_payment 에러 해결

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -26,3 +26,25 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",  # 디버그 레벨로 설정
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": "DEBUG",
+            "handlers": ["console"],
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "DEBUG",  # 요청 관련 로그
+            "propagate": True,
+        },
+    },
+}
