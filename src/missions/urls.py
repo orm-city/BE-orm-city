09a9d186e84289c47c30c94ad234c72adeedbbc8
geza <geza@baegseunghyeon-ui-MacBookAir.local>
2024-10-07 16:13:20 +0900

    mixin으로 view 변경

--- src/missions/urls.py
+++ src/missions/urls.py
@@ -1,26 +1,47 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import (
-    MissionViewSet,
-    QuestionViewSet,
-    MultipleChoiceOptionViewSet,
-    CodeQuestionViewSet,
-    MissionSubmissionViewSet,
-    MultipleChoiceSubmissionViewSet,
-    CodeSubmissionViewSet,
+    MissionListCreateView,
+    MissionRetrieveUpdateDestroyView,
+    QuestionListCreateView,
+    QuestionRetrieveUpdateDestroyView,
+    MultipleChoiceOptionListCreateView,
+    MultipleChoiceOptionRetrieveUpdateDestroyView,
+    CodeQuestionListCreateView,
+    CodeQuestionRetrieveUpdateDestroyView,
+    MissionSubmissionListCreateView,
+    MissionSubmissionRetrieveView,
+    MultipleChoiceSubmissionListCreateView,
+    MultipleChoiceSubmissionRetrieveView,
+    CodeSubmissionListCreateView,
+    CodeSubmissionRetrieveView,
 )
 
-# DefaultRouter를 사용하여 뷰셋 자동 라우팅
-router = DefaultRouter()
-router.register(r'missions', MissionViewSet, basename='mission')
-router.register(r'questions', QuestionViewSet, basename='question')
-router.register(r'multiple-choice-options', MultipleChoiceOptionViewSet, basename='multiplechoiceoption')
-router.register(r'code-questions', CodeQuestionViewSet, basename='codequestion')
-router.register(r'mission-submissions', MissionSubmissionViewSet, basename='missionsubmission')
-router.register(r'multiple-choice-submissions', MultipleChoiceSubmissionViewSet, basename='multiplechoicesubmission')
-router.register(r'code-submissions', CodeSubmissionViewSet, basename='codesubmission')
-
-# URL 패턴 정의
 urlpatterns = [
-    path('', include(router.urls)),
+    # 미션 엔드포인트
+    path('missions/', MissionListCreateView.as_view(), name='mission-list-create'),
+    path('missions/<int:pk>/', MissionRetrieveUpdateDestroyView.as_view(), name='mission-detail'),
+
+    # 문제 엔드포인트
+    path('questions/', QuestionListCreateView.as_view(), name='question-list-create'),
+    path('questions/<int:pk>/', QuestionRetrieveUpdateDestroyView.as_view(), name='question-detail'),
+
+    # 객관식 선택지 엔드포인트
+    path('multiple-choice-options/', MultipleChoiceOptionListCreateView.as_view(), name='mc-option-list-create'),
+    path('multiple-choice-options/<int:pk>/', MultipleChoiceOptionRetrieveUpdateDestroyView.as_view(), name='mc-option-detail'),
+
+    # 코드 문제 추가 정보 엔드포인트
+    path('code-questions/', CodeQuestionListCreateView.as_view(), name='code-question-list-create'),
+    path('code-questions/<int:pk>/', CodeQuestionRetrieveUpdateDestroyView.as_view(), name='code-question-detail'),
+
+    # 미션 제출 엔드포인트
+    path('mission-submissions/', MissionSubmissionListCreateView.as_view(), name='mission-submission-list-create'),
+    path('mission-submissions/<int:pk>/', MissionSubmissionRetrieveView.as_view(), name='mission-submission-detail'),
+
+    # 객관식 문제 제출 엔드포인트
+    path('multiple-choice-submissions/', MultipleChoiceSubmissionListCreateView.as_view(), name='mc-submission-list-create'),
+    path('multiple-choice-submissions/<int:pk>/', MultipleChoiceSubmissionRetrieveView.as_view(), name='mc-submission-detail'),
+
+    # 코드 문제 제출 엔드포인트
+    path('code-submissions/', CodeSubmissionListCreateView.as_view(), name='code-submission-list-create'),
+    path('code-submissions/<int:pk>/', CodeSubmissionRetrieveView.as_view(), name='code-submission-detail'),
 ]
