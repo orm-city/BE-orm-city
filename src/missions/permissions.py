b2b892c3d05e3298bcf2cf4407f10dc5f6c49b12
KyungMin.Ko <rhrudals312@naver.com>
2024-10-11 16:44:57 +0900

    dev cofilct 해결할 때 실수 생긴 것 수정, import 경로 수정

--- src/missions/permissions.py
+++ src/missions/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from .models import Enrollment
+from courses.models import Enrollment
 
 
 class IsActiveOrCompletedEnrollmentOrManagerAdmin(permissions.BasePermission):
