3e9a39c0ba82522961cb9209c69187b107822069
KyungMin.Ko <rhrudals312@naver.com>
2024-10-14 11:30:15 +0900

    ORMC-66 cors설정 변경

--- src/config/settings/docker.py
+++ src/config/settings/docker.py
@@ -6,6 +6,8 @@ ALLOWED_HOSTS = ["*"]  # noqa
 
 CORS_ALLOWED_ORIGINS = [
     "http://3.36.122.224",  # 브라우저에서 요청을 보낸 도메인
+    "http://orm-city.site",
+    "https://orm-city.site",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
